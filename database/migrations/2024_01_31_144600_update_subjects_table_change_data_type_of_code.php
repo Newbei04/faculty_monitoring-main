e226d3b1425684e6acf6fd935af9629623fd7d58
Newbei04 <rmredenrudolfrecto10@gmail.com>
2026-04-20 00:41:44 +0800

    update migration

--- database/migrations/2024_01_31_144600_update_subjects_table_change_data_type_of_code.php
+++ database/migrations/2024_01_31_144600_update_subjects_table_change_data_type_of_code.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('code')->unique(); // 🔥 prevents duplicates
+            $table->string('code')->unique(); 
             $table->timestamps();
         });
     }
