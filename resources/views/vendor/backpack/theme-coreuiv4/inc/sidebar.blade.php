a1ae3a10f27edea138325d501d1e609a827778a4
Newbei04 <rmredenrudolfrecto10@gmail.com>
2026-04-20 00:10:13 +0800

    update header and  security

--- resources/views/vendor/backpack/theme-coreuiv4/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/theme-coreuiv4/inc/sidebar.blade.php
@@ -1,9 +1,15 @@
 @if (backpack_auth()->check())
 <div class="{{ backpack_theme_config('classes.sidebar') }}" id="sidebar">
     <div class="sidebar-brand d-none d-md-flex">
-        <a class="navbar-brand fs-6 text-decoration-none text-uppercase" href="{{ backpack_url('dashboard') }}" title="{{ backpack_theme_config('project_name') }}">
-            <img src="/image/logo.png" alt="PSU logo" class="me-2" style="height:24px; width:auto;">
-            {!! backpack_theme_config('project_logo') !!}
+        <a class="navbar-brand text-decoration-none" href="{{ backpack_url('dashboard') }}" title="{{ backpack_theme_config('project_name') }}">
+            <div class="d-flex align-items-center gap-2">
+                <img src="/image/logo.png" alt="PSU logo" style="height:50px;">
+                <div class="d-flex flex-column lh-sm">
+                    <span class="text-uppercase" style="font-family: 'Anton', sans-serif; font-size: 1.6rem; letter-spacing: 2px;">PSU</span>
+                    <span style="font-size: 0.55rem; font-weight: 600; letter-spacing: 1px;">PANGASINAN STATE UNIVERSITY</span>
+                    <span class="text-white-50" style="font-size: 1rem; font-style: italic;">College of CHMBAC</span>
+                </div>
+            </div>
         </a>
     </div>
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="init">
