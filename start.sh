a5045f9f047d4064213c30651ff582b6a6395f54
Newbei04 <rmredenrudolfrecto10@gmail.com>
2026-04-22 17:49:24 +0800

    update after migration stop migration

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Waiting for database..."
-until php artisan db:show > /dev/null 2>&1; do
-  echo "Retrying in 5 seconds..."
-  sleep 5
-done
+# echo "Waiting for database..."
+# until php artisan db:show > /dev/null 2>&1; do
+#   echo "Retrying in 5 seconds..."
+#   sleep 5
+# done
 
-echo "Dropping and recreating all tables..."
-php artisan migrate:fresh --force
+# echo "Dropping and recreating all tables..."
+# php artisan migrate:fresh --force
 
-echo "Running seeder..."
-php artisan db:seed --force
+# echo "Running seeder..."
+# php artisan db:seed --force
 
 echo "Clearing caches..."
 php artisan config:cache
